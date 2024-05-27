b0c773d75a5ebd3d7ecfdc092f5990c3f868dc3e
Dima Dobronovich <dobronovich_95@mail.ru>
2024-05-27 17:49:49 +0700

    feat: добавил черновую сортировку, поправил частично верстку карточек специальностей

--- src/components/Main/Specialization/Specialization.jsx
+++ src/components/Main/Specialization/Specialization.jsx
@@ -1,24 +1,64 @@
 import './Specialization.scss';
+import {useState, useEffect, useMemo} from 'react';
 import SpecializationCard from './SpecializationCard';
 function Specialization({faculties}) {
-    console.log(faculties);
+  const [facultiesRender, setFacultiesRender] = useState([]);
+  const [addMore, setAddMore] = useState(5);
+
+  useEffect(() => {
+    setFacultiesRender(faculties.slice(0, addMore));
+  }, [faculties, addMore]);
+
+  function addFacultiesRender() {
+    setAddMore(addMore + 5);
+    setFacultiesRender(faculties.slice(0, addMore));
+  }
+
+  function resort(selector, parametr) {
+    const nodeList = document.querySelectorAll(selector);
+    const dict = {};
+    const parent = nodeList[0].parentNode;
+    nodeList.forEach(node => {
+      const key = node.querySelector(parametr).innerText.trim().replace(/\D/g, '');
+      dict[key] = node;
+    });
+    const keys = Object.keys(dict);
+    // console.log(keys);
+    // console.log(Object.keys(dict));
+    keys.sort((a, b) => a - b).forEach(k => parent.appendChild(dict[k]));
+  }
+
   return (
     <div class="specialization">
       <div class="specialization-header">
         <h2 class="specialization-title boldTxt">Специальности</h2>
-        <span class="specialization-subtile smallTxtOS">432 специальности</span>
+        <span class="specialization-subtile smallTxtOS">{`${faculties.length} ${
+          faculties.length > 4 ? 'специальностей' : 'специальности'
+        } `}</span>
       </div>
       <div class="specialization-filter">
         <ul>
           <span>Сортировать:</span>
           <li>
-            <button id="payment">по оплате в год</button>
+            <button
+              onClick={() => {
+                resort('.specialization__card', '.payment');
+              }}
+              id="payment">
+              по оплате в год
+            </button>
           </li>
           <li>
             <button id="2">по отзывам</button>
           </li>
           <li>
-            <button id="points">по прозодному баллу</button>
+            <button
+              id="points"
+              onClick={() => {
+                resort('.specialization__card', '.points');
+              }}>
+              по проходному баллу
+            </button>
           </li>
           <li>
             <button id="places">по количеству бюджетных мест</button>
@@ -27,13 +67,17 @@ function Specialization({faculties}) {
       </div>
       <div class="specialization-body">
         <div class="specialization-row">
-          {faculties.map(faculty => (
-            <SpecializationCard faculty={faculty} key={faculty.id}/>
+          {facultiesRender.map(faculty => (
+            <SpecializationCard faculty={faculty} key={faculty.id} />
           ))}
         </div>
-        <div class="specialization-link">
-          <button class="btn whiteBlue">Показать все специальности</button>
-        </div>
+        {facultiesRender.length < faculties.length && (
+          <div class="specialization-link">
+            <button onClick={addFacultiesRender} class="btn whiteBlue">
+              Показать еще специальности
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Main/Specialization/SpecializationCard.jsx
+++ src/components/Main/Specialization/SpecializationCard.jsx
@@ -1,54 +1,59 @@
-import './Specialization.scss';
+import {useState} from 'react';
+import './SpecializationCard.scss';
+import cardPhoto from '../../../img/Tech.jpg';
+import icon__bank from '../../../img/icon__bank.svg';
+import icon__flag from '../../../img/icon__flag.svg';
+import icon__bed from '../../../img/icon__bed.svg';
 function SpecializationCard({faculty}) {
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  const [price, setPrice] = useState(Math.round(getRandom(50000, 150000) / 1000) * 1000);
+  const [score, setScore] = useState(getRandom(100, 200));
+  const [place, setPlace] = useState(getRandom(20, 50));
   return (
-    <div class="specialization-card">
-      <div class="specialization-item itemImg">
-        <picture>
-          <source srcset="./images/specialization/library.jpg" media="(max-width: 768px)" />
-          <img src="./images/cardPhoto.jpg" alt="" />
-        </picture>
+    <div className="specialization__card">
+      <div className="specialization__card-section">
+        <img className="specialization__card-photo" src={cardPhoto} alt="" />
       </div>
-      <div class="specialization-item _line borderRight">
-        <div class="specialization-code">бакалавриат · 01.03.00</div>
-        <div class="specialization-name">{faculty.name}</div>
-        <div class="specialization-format">
+      <div className="specialization__card-section">
+        <div className="specialization-code">бакалавриат · 01.03.00</div>
+        <div className="specialization-name">{faculty.name}</div>
+        <div className="specialization-format">
           <span>Очно</span>
           <span>Заочно</span>
           <span>Очно-заочно</span>
           <span>Дистанционно</span>
         </div>
-        <div class="specialization-description">
-          Программное обеспечение вычислительных систем · Математическое и программное обеспечение защиты информации · Экономика ·
-          Организация педагогическо...
-        </div>
-        <div class="specialization-all">
-          <a href="#">Все 43 профиля</a>
+        <div className="specialization-description">
+          Перечень специализаций институтата/факультета · Перечень направлений/специализаций институтата/факультета · Перечень
+          направлений/специализаций институтата/факультета
         </div>
       </div>
-      <div class="specialization-item rowMobile">
-        <div class="specialization-icons">
-          <div class="icon grey">
-            <img src="./images/flag.svg" alt="" />
+      <div className="specialization__card-section">
+        <div className="specialization-icons">
+          <div className="icon grey">
+            <img src={icon__bank} alt="" />
           </div>
-          <div class="icon grey">
-            <img src="./images/bank.svg" alt="" />
+          <div className="icon grey">
+            <img src={icon__flag} alt="" />
           </div>
-          <div class="icon grey">
-            <img src="./images/Zzz.svg" alt="" />
+          <div className="icon grey">
+            <img src={icon__bed} alt="" />
           </div>
         </div>
 
-        <div class="specialization-line">
-          <span class="line-text specialization-line_text  smallTxtOS">Бюджетных мест</span>
-          <span class="line-text specialization-line_text mediumTxtFormular places">260</span>
+        <div className="specialization-line">
+          <span className="line-text specialization-line_text  smallTxtOS">Бюджетных мест</span>
+          <span className="line-text specialization-line_text mediumTxtFormular places">{place}</span>
         </div>
-        <div class="specialization-line">
-          <span class="line-text specialization-line_text  smallTxtOS">Проходной балл</span>
-          <span class="line-text specialization-line_text points mediumTxtFormular">152</span>
+        <div className="specialization-line">
+          <span className="line-text specialization-line_text  smallTxtOS">Проходной балл</span>
+          <span className="line-text specialization-line_text points mediumTxtFormular">{score}</span>
         </div>
-        <div class="specialization-line">
-          <span class="line-text specialization-line_text  smallTxtOS">Оплата в год</span>
-          <span class="line-text specialization-line_text mediumTxtFormular payment">от 90 000 &#8381</span>
+        <div className="specialization-line">
+          <span className="line-text specialization-line_text  smallTxtOS">Оплата в год</span>
+          <span className="line-text specialization-line_text mediumTxtFormular payment">от {price} &#8381;</span>
         </div>
       </div>
     </div>
